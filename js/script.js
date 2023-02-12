9b66c96e3e8bd6990dbbf87d88c6334521ceefa4
Sang Dang <sangdase170287@fpt.edu.vn>
2023-02-12 23:57:00 +0700

    Finish

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ $("body>header>nav>.toggle").click(function () {
 
 // Main Carousel buttons Mobile
 
-const slideContainer = $("#slide-container")[0];
+const slideContainer = $(".slide-container")[0];
 const slides = Array.from($(".slide"));
-const prevBtn = $("#slide-arrow-prev")[0];
-const nextBtn = $("#slide-arrow-next")[0];
+const prevBtn = $(".slide-arrow-prev")[0];
+const nextBtn = $(".slide-arrow-next")[0];
 
 let currentSlide = 0;
 
@@ -39,7 +39,7 @@ $(".shopping-cart").click(function () {
 
 // desktop slide switch
 
-const slideDisplay = document.querySelector("#current-slide");
+const slideDisplay = document.querySelector(".current-slide");
 const displayImg = slideDisplay.querySelector("img");
 const targetImg = document.querySelector(".slide.active img");
 displayImg.src = targetImg.src;
@@ -88,3 +88,79 @@ amountInput.addEventListener('blur', () => {
         amountInput.value = "0";
     }
 })
+
+// Lightbox functionality
+
+function toggleLightbox() {
+    if (window.matchMedia("(min-width: 1000px)")) {
+        $(window).scrollTop(0);
+        if ($("#main-lightbox").hasClass("active")) {
+            $("#main-lightbox").empty();
+            $("#main-lightbox").removeClass("active");
+        } else {
+            $(".slide-wrapper:first").clone().appendTo("#main-lightbox");
+            $("#main-lightbox").addClass("active");
+            $("#close-lightbox").clone().appendTo("#main-lightbox > .slide-wrapper");
+        }
+    }
+}
+
+// main toggle
+var current;
+$(".current-slide").click(() => {
+    toggleLightbox();
+    document.querySelectorAll("#main-lightbox .slide").forEach(slide => {
+        changeLightboxImage(slide);
+    })
+
+    let slides = document.querySelectorAll("#main-lightbox .slide");
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains("active")) {
+            current = i;
+            break;
+        }
+    }
+
+    lightboxButtons();
+})
+
+// lightbox buttons 
+
+function changeLightboxImage(slide) {
+    slide.addEventListener('click', () => {
+        let slides = document.querySelectorAll("#main-lightbox .slide");
+
+        document.querySelector("#main-lightbox .current-slide img").src = slide.children[0].children[1].src;
+        slides.forEach(item => item.classList.remove("active"));
+        slide.classList.add("active");
+        for (let i = 0; i < slides.length; i++) {
+            if (slides[i].classList.contains("active")) {
+                current = i;
+                break;
+            }
+        }
+    })
+}
+
+function lightboxButtons() {
+    let slides = document.querySelectorAll("#main-lightbox .slide");
+    document.querySelector("#main-lightbox .slide-arrow-next").addEventListener('click', () => {
+        current = (current + 1) % slides.length;
+        document.querySelector("#main-lightbox .current-slide img").src = slides[current].children[0].children[1].src;
+        slides.forEach(item => item.classList.remove("active"));
+        slides[current].classList.add("active");
+        console.log(current);
+    });
+    document.querySelector("#main-lightbox .slide-arrow-prev").addEventListener('click', () => {
+        current = (current - 1) % slides.length;
+        if (current == -1) {
+            current = 3;
+        }
+
+        console.log(-1 % 4);
+        document.querySelector("#main-lightbox .current-slide img").src = slides[current].children[0].children[1].src;
+        slides.forEach(item => item.classList.remove("active"));
+        slides[current].classList.add("active");
+        console.log(current);
+    });
+}
